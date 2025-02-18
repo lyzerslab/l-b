6a23347f53bb8965801c8474fd19e922a5ec39f7
zkrana <zkranao@gmail.com>
2025-02-18 14:03:40 +0600

    Package plan changed and also api and headers .

--- auth/backend-assets/formSubmit.php
+++ auth/backend-assets/formSubmit.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://lyzerslab.com");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type");
 
